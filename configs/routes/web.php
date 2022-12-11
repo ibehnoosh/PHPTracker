7fbfc341487ee5d58d4e7a0b07947d339cd77175
Behnoosh Moshtagh <behnoosh.moshtagh@gmail.com>
2022-12-11 23:59:38 +0330

    Add Auth Frontend

--- configs/routes/web.php
+++ configs/routes/web.php
@@ -2,13 +2,14 @@
 
 declare(strict_types = 1);
 
+use App\Controllers\AuthController;
 use App\Controllers\HomeController;
 use Slim\App;
 
 return function (App $app) {
     $app->get('/', [HomeController::class, 'index']);
-    $app->get('/login',[AutuController::class,'loginView']);
-    $app->get('/register',[AutuController::class,'registerView']);
-    $app->post('/login',[AutuController::class,'loginIn']);
-    $app->post('/register',[AutuController::class,'register']);
+    $app->get('/login',[AuthController::class,'loginView']);
+    $app->get('/register',[AuthController::class,'registerView']);
+    $app->post('/login',[AuthController::class,'logIn']);
+    $app->post('/register',[AuthController::class,'register']);
 };
