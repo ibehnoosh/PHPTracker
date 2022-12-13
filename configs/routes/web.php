48a8d9dc115dee88ef51f4bec108c8e72cd4a48f
Behnoosh Moshtagh <behnoosh.moshtagh@gmail.com>
2022-12-13 22:29:15 +0330

    FInish Auth

--- configs/routes/web.php
+++ configs/routes/web.php
@@ -3,14 +3,28 @@
 declare(strict_types = 1);
 
 use App\Controllers\AuthController;
+use App\Controllers\CategoriesController;
 use App\Controllers\HomeController;
+use App\Middleware\AuthMiddleware;
+use App\Middleware\GuestMiddleware;
 use Slim\App;
+use Slim\Routing\RouteCollectorProxy;
 
 return function (App $app) {
-    $app->get('/', [HomeController::class, 'index']);
+    $app->get('/', [HomeController::class, 'index'])->add(AuthMiddleware::class);
 
-    $app->get('/login', [AuthController::class, 'loginView']);
-    $app->get('/register', [AuthController::class, 'registerView']);
-    $app->post('/login', [AuthController::class, 'logIn']);
-    $app->post('/register', [AuthController::class, 'register']);
+    $app->group('', function (RouteCollectorProxy $guest) {
+        $guest->get('/login', [AuthController::class, 'loginView']);
+        $guest->get('/register', [AuthController::class, 'registerView']);
+        $guest->post('/login', [AuthController::class, 'logIn']);
+        $guest->post('/register', [AuthController::class, 'register']);
+    })->add(GuestMiddleware::class);
+
+    $app->post('/logout', [AuthController::class, 'logOut'])->add(AuthMiddleware::class);
+
+    $app->group('/categories', function (RouteCollectorProxy $categories) {
+        $categories->get('', [CategoriesController::class, 'index']);
+        $categories->post('', [CategoriesController::class, 'store']);
+        $categories->delete('/{id}', [CategoriesController::class, 'delete']);
+    })->add(AuthMiddleware::class);
 };
